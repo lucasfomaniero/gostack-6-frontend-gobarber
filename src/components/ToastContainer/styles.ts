e4f640194ed4d454812811f06ddf61ca24bc2aee
Lucas Maniero <lucasfomaniero@gmail.com>
2020-11-21 01:31:28 -0300

    feat Minor improvements in Toast component

---
+++ src/components/ToastContainer/styles.ts
@@ -0,0 +1,9 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding: 16px 16px 16px 16px;
+    overflow: hidden;
+`;
