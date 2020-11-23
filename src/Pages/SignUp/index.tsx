278f6dc11d09164eea4741b676bc6b0def418c7d
Lucas Maniero <lucasfomaniero@gmail.com>
2020-11-22 21:27:04 -0300

    Animation and route improvements

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -48,37 +48,39 @@ const SignUp: React.FC = () => {
             <Container>
                 <Background />
                 <Content>
-                    <img src={logoImg} alt="GoBarber Logo" />
+                    <AnimationContainer>
+                        <img src={logoImg} alt="GoBarber Logo" />
 
-                    <Form ref={formRef} onSubmit={handleSubmit}>
-                        <h1>Faça seu cadastro</h1>
-                        <Input
-                            type="text"
-                            icon={FiUser}
-                            name="name"
-                            id="name"
-                            placeholder="Nome"
-                        />
-                        <Input
-                            type="email"
-                            icon={FiMail}
-                            name="email"
-                            id="email"
-                            placeholder="E-mail"
-                        />
-                        <Input
-                            type="password"
-                            icon={FiLock}
-                            name="password"
-                            id="password"
-                            placeholder="Senha"
-                        />
-                        <Button type="submit">Cadastrar</Button>
-                        <Link to="/">
-                            <FiArrowLeft size={20} />
-                            Voltar para o Login
-                        </Link>
-                    </Form>
+                        <Form ref={formRef} onSubmit={handleSubmit}>
+                            <h1>Faça seu cadastro</h1>
+                            <Input
+                                type="text"
+                                icon={FiUser}
+                                name="name"
+                                id="name"
+                                placeholder="Nome"
+                            />
+                            <Input
+                                type="email"
+                                icon={FiMail}
+                                name="email"
+                                id="email"
+                                placeholder="E-mail"
+                            />
+                            <Input
+                                type="password"
+                                icon={FiLock}
+                                name="password"
+                                id="password"
+                                placeholder="Senha"
+                            />
+                            <Button type="submit">Cadastrar</Button>
+                            <Link to="/">
+                                <FiArrowLeft size={20} />
+                                Voltar para o Login
+                            </Link>
+                        </Form>
+                    </AnimationContainer>
                 </Content>
             </Container>
         </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import SignIn from '../Pages/SignIn';
 import SignUp from '../Pages/SignUp';
+import Route from './Route';
+import Dashboard from '../Pages/Dashboard';
 
 const Routes: React.FC = () => (
     <Switch>
         <Route path="/" exact component={SignIn} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
 );
 
